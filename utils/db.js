026f7af5cbf6c67e9b48e12b6630976028417377
Novian Rizky <79080026+itsNRizky@users.noreply.github.com>
2022-10-20 22:25:03 +0700

    Using another DB for 2.0

--- utils/db.js
+++ utils/db.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://iotanic:iotanic123@nrizky-student.g1cl38b.mongodb.net/iotanic_app?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://iotanic:iotanic123@nrizky-student.g1cl38b.mongodb.net/iotanic_api?retryWrites=true&w=majority')
